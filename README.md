a0a3254e779811b62f72de4156931e02b1f525b4
zlterry68 <2019230899@unfv.edu.pe>
2022-11-22 23:18:41 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Crud Listado de Tareas pendientes
+ Proyecto de listado de tareas(anotaciones) que te permite marcar tareas como completado o no completado.
+
+### Notas
+Recuerden reconstruir los módulos del Node
+
+```
+npm install
+```
+
+Y para construir el build, recuerden:
+```
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles.css';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+
+
+export const todoList = new TodoList();
+
+
+
+
+
+todoList.todos.forEach(crearTodoHtml); 
+
+// const newTodo = new Todo('Aprender JavaScript');
+// todoList.nuevoTodo(newTodo);
+// todoList.todos[0].imprimirClase();
+// newTodo.imprimirClase();
+
+
+console.log('todos',todoList.todos);
